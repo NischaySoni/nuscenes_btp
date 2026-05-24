3d86ee4c3d18be8929a0519d2365c991ee5f18fd
NischaySoni <nischay23353@iiitd.ac.in>
2026-05-25 00:05:04 +0530

    bert_multihead

--- run.py
+++ run.py
@@ -39,6 +39,7 @@ def parse_args():
                            'mcan_trimodal_v12_map',
                            'mcan_trimodal_v13_bert_map',
                            'mcan_trimodal_v14_bert_ft',
+                           'mcan_trimodal_v15_bert_mh',
                            'mcan_centerpoint_fusion',
                            'mcan_centerpoint',
                            'mcan_large',
